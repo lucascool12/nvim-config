6ba55916c050a866d9dd5923e721d09c410c33ed
Lucas Van Laer <lucas.van.laer@gmail.com>
2022-12-23 12:02:12 +0100

    treesitter indentation temp fix

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -3,10 +3,11 @@ require'nvim-treesitter.configs'.setup {
 
   highlight = {
     enable = true,
+    additional_vim_regex_highlighting = true,
   },
-  indent = {
-    enable = true,
-  },
+  -- indent = {
+  --   enable = true,
+  -- },
 }
 
 -- -- temp solution to treesitter indentation not turning off
