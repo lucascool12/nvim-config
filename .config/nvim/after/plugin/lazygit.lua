452093ce582c78b23f7ab5d755c504a54e8d0c0b
Lucas Van Laer <lucas.van.laer@gmail.com>
2022-12-28 14:22:17 +0100

    rm unused file imporve yadm lazygit

--- .config/nvim/after/plugin/lazygit.lua
+++ .config/nvim/after/plugin/lazygit.lua
@@ -38,7 +38,8 @@ local lazygit_yadm = Terminal:new(lazygit_yadm_config)
 
 function Lazygit_toggle()
   vim.fn.system("git rev-parse --is-inside-work-tree")
-  if (vim.v.shell_error ~= 0) and vim.loop.cwd():find(vim.fs.normalize('~/.'), 1, true) == 1 then
+  if ((vim.v.shell_error ~= 0) and vim.loop.cwd():find(vim.fs.normalize('~/.'), 1, true) == 1)
+    or (vim.loop.cwd() == vim.fs.normalize('~/'):sub(1, -2)) then
     lazygit_yadm:toggle()
   else
     lazygit:toggle()
