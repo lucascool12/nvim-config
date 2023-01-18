be14d80d2ea2cad3b6f2bbc4ce3d81917209c93e
Lucas Van Laer <lucas.van.laer@gmail.com>
2023-01-18 12:58:32 +0100

    blend cmp completion & doc

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,2 +1,22 @@
+require'nightfox'.setup{
+  options = {
+    transparent = false,
+  },
+  specs = {
+    all = {
+      syntax = {
+        variable = "cyan",
+      },
+    },
+  },
+  groups = {
+    all = {
+      Pmenu = { bg = "NONE" },
+      NormalFloat = { bg = "NONE" },
+      NormalNC = { bg = "NONE" },
+      WildMenu = { bg = "NONE" },
+    },
+  },
+}
+vim.opt.pumblend = 19
 vim.cmd("colorscheme terafox")
-vim.cmd([[highlight @variable guifg=LightBlue]])
