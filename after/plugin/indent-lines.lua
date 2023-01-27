df9bc653153adeee644251cb7260bdf7735f8236
Lucas Van Laer <lucas.van.laer@gmail.com>
2023-01-27 21:56:34 +0100

    indentation lines

---
+++ after/plugin/indent-lines.lua
@@ -0,0 +1,16 @@
+function Set (list)
+  local set = {}
+  for _, l in ipairs(list) do set[l] = true end
+  return set
+end
+vim.g.indentLine_enabled = 0
+vim.api.nvim_create_autocmd({"BufWinEnter"},{
+  callback = function (ev)
+    if vim.api.nvim_buf_get_option(ev.buf, "filetype") == "toggleterm" then
+      return
+    end
+    if vim.api.nvim_buf_get_option(ev.buf, "buftype") == "" then
+      vim.cmd("IndentLinesEnable")
+    end
+  end,
+})
