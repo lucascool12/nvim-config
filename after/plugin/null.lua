08138fb1d0892d9f9b13d65a04061930c2dad670
Lucas Van Laer <lucas.van.laer@gmail.com>
2023-02-21 21:59:50 +0100

    change: flake8 to ruff

--- after/plugin/null.lua
+++ after/plugin/null.lua
@@ -8,17 +8,16 @@ null_ls.setup{
         update_in_insert = false,
       },
     },
-    null_ls.builtins.diagnostics.flake8.with(
-      (function ()
-        local args = null_ls.builtins.diagnostics.flake8._opts.args
-        for index, arg in ipairs(flake8_config) do
-          table.insert(args, index, arg)
-        end
-        return {
-          args = args,
-        }
-      end)()
-    ),
-    null_ls.builtins.diagnostics.mypy,
+    -- null_ls.builtins.diagnostics.flake8.with(
+    --   (function ()
+    --     local args = null_ls.builtins.diagnostics.flake8._opts.args
+    --     for index, arg in ipairs(flake8_config) do
+    --       table.insert(args, index, arg)
+    --     end
+    --     return {
+    --       args = args,
+    --     }
+    --   end)()
+    -- ),
   },
 }
