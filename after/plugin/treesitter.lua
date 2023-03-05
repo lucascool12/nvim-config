ee875c7fdc07e89320fbc818e01454430c8a0f12
Lucas Van Laer <lucas.van.laer@gmail.com>
2023-03-05 15:39:55 +0100

    feat: test xml parser

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -13,8 +13,15 @@ require'nvim-treesitter.configs'.setup {
   -- },
 }
 
+local parser_config = require'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.xml = {
+  install_info = {
+    url = "https://github.com/lucascool12/tree-sitter-xml.git", -- local path or git repo
+    files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
+  },
+}
 local parser_mapping = require("nvim-treesitter.parsers").filetype_to_parsername
-parser_mapping.xml = "html" -- map the html parser to be used when using xml files
+-- parser_mapping.xml = "html" -- map the html parser to be used when using xml files
 
 -- -- temp solution to treesitter indentation not turning off
 -- vim.cmd([[
